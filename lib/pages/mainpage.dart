f92637f973ca1e4a85782de193b028d7e63fdb84
Asamoah Yeboah Felix <109028884+kelvin-kush@users.noreply.github.com>
2022-11-01 14:01:27 -0700

    heading

--- lib/pages/mainpage.dart
+++ lib/pages/mainpage.dart
@@ -37,7 +37,7 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CBS'),
+        title: Text('CBS Stories'),
       ),
       body: pages[indexclicked],
       drawer: Drawer(
